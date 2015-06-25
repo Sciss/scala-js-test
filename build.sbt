bd17d1bc8e5fdf6c0d51ea06336c15a5c2b3fdd6
Hanns Holger Rutz <contact@sciss.de>
2015-06-25 14:49:38 +0200

    test jquery

--- build.sbt
+++ build.sbt
@@ -4,4 +4,6 @@ scalaVersion := "2.11.6"
 
 // libraryDependencies += "de.sciss" %% "scalacollider" % "1.17.3"
 
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
+
 enablePlugins(ScalaJSPlugin)
--- src/main/scala/de/sciss/scalajstest/Main.scala
+++ src/main/scala/de/sciss/scalajstest/Main.scala
@@ -2,9 +2,18 @@ package de.sciss.scalajstest
 
 import scala.scalajs.js
 import scala.scalajs.js.JSApp
+import org.scalajs.jquery.jQuery
+
+import scala.scalajs.js.annotation.JSExport
 
 object Main extends JSApp {
-  def main(): Unit = {
+  def main(): Unit = ()
+
+  @JSExport
+  def addClickedMessage(): Unit =
+    jQuery("body").append("<p>Hello World</p>")
+
+  def makeSound(): Unit = {
     val ctx = js.Dynamic.newInstance(js.Dynamic.global.AudioContext)()
     println(s"AudioContext state = ${ctx.state}")
     val o = ctx.createOscillator()
