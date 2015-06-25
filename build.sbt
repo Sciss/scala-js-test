2b9194c225db7d101f3cec293e0ece6468fdb1f6
Hanns Holger Rutz <contact@sciss.de>
2015-06-25 14:16:50 +0200

    update libs

--- build.sbt
+++ build.sbt
@@ -1,5 +1,7 @@
-scalaJSSettings
-
 name := "Scala.js Test"
 
-scalaVersion := "2.11.4"
+scalaVersion := "2.11.6"
+
+// libraryDependencies += "de.sciss" %% "scalacollider" % "1.17.3"
+
+enablePlugins(ScalaJSPlugin)
--- src/main/scala/de/sciss/scalajstest/Main.scala
+++ src/main/scala/de/sciss/scalajstest/Main.scala
@@ -5,11 +5,13 @@ import scala.scalajs.js.JSApp
 
 object Main extends JSApp {
   def main(): Unit = {
-    println("Hello world!")
     val ctx = js.Dynamic.newInstance(js.Dynamic.global.AudioContext)()
+    println(s"AudioContext state = ${ctx.state}")
     val o = ctx.createOscillator()
     o.frequency.value = 666
-    // o.start(0)
-    // o.connect(ctx.destination)
+    // ctx.resume()
+    o.connect(ctx.destination)
+    o.start()
+    println("Done")
   }
 }
