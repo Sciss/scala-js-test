955638b1358109e0e08dca1ff893d18b09ef2972
Hanns Holger Rutz <contact@sciss.de>
2015-06-25 15:36:07 +0200

    html5 audio tag

--- src/main/scala/de/sciss/scalajstest/Main.scala
+++ src/main/scala/de/sciss/scalajstest/Main.scala
@@ -1,17 +1,28 @@
 package de.sciss.scalajstest
 
-import scala.scalajs.js
-import scala.scalajs.js.JSApp
+import org.scalajs.dom.raw.HTMLAudioElement
 import org.scalajs.jquery.jQuery
 
-import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js
+import scala.scalajs.js.JSApp
 
 object Main extends JSApp {
-  def main(): Unit = ()
+  def main(): Unit =
+    jQuery(setupUI _)
 
-  @JSExport
+  def setupUI(): Unit = {
+    jQuery("#play-button").click(play _)
+    jQuery("#stop-button").click(stop _)
+    // jQuery("body").append("<p>Hello World</p>")
+  }
+
   def addClickedMessage(): Unit =
     jQuery("body").append("<p>Hello World</p>")
+
+  def player: HTMLAudioElement = jQuery("#player")(0).asInstanceOf[HTMLAudioElement]
+
+  def play(): Unit = player.play()
+  def stop(): Unit = player.pause()
 
   def makeSound(): Unit = {
     val ctx = js.Dynamic.newInstance(js.Dynamic.global.AudioContext)()
